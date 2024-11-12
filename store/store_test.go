51de94b2f5b7c30160d17bdf0279c01537219596
James Mason <james.mason@bjss.com>
2024-11-12 15:04:47 +0000

    fix benchmark tests and improve test frameworks

--- store/store_test.go
+++ store/store_test.go
@@ -2,21 +2,68 @@ package store
 
 import (
 	"reflect"
+	"strconv"
+	"sync"
 	"testing"
 
 	"github.com/google/uuid"
 )
 
-func BenchmarkStore(b *testing.B) {
+func BenchmarkAddItemEmptyStore(b *testing.B) {
 	store := CreateAndStartStore(nil)
+	var wg sync.WaitGroup
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		AddItem(&store, "banana ", "Medium")
-		EditPriority(&store, uuid.New(), "High")
-		EditTitle(&store, uuid.New(), "ubunga")
-		ToggleComplete(&store, uuid.New())
-		GetAllItems(&store)
-		DeleteItem(&store, uuid.New())
+		wg.Add(1)
+		go func() {
+			AddItem(&store, "banana", HighPriority)
+			wg.Done()
+		}()
 	}
+	wg.Wait()
+}
+
+func BenchmarkAddItemPopulatedStore(b *testing.B) {
+	startList := createToDoListWithItems(b.N, uuid.New())
+	store := CreateAndStartStore(startList)
+	var wg sync.WaitGroup
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		wg.Add(1)
+		go func() {
+			AddItem(&store, "banana", HighPriority)
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+}
+
+func BenchmarkEditItem(b *testing.B) {
+	startList := createToDoListWithItems(b.N, uuid.New())
+	store := CreateAndStartStore(startList)
+	idChan := make(chan uuid.UUID, b.N)
+	for id := range store.toDoList {
+		idChan <- id
+	}
+	var wg sync.WaitGroup
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		wg.Add(3)
+		itemID := <-idChan
+		go func() {
+			EditTitle(&store, itemID, "new Title")
+			wg.Done()
+		}()
+		go func() {
+			EditPriority(&store, itemID, MediumPriority)
+			wg.Done()
+		}()
+		go func() {
+			ToggleComplete(&store, itemID)
+			wg.Done()
+		}()
+	}
+	wg.Wait()
 }
 
 func TestDeleteItem(t *testing.T) {
@@ -26,9 +73,9 @@ func TestDeleteItem(t *testing.T) {
 		id   uuid.UUID
 		want error
 	}{
-		{name: "happy path", list: createToDoListWithItems(), id: knownID, want: nil},
+		{name: "happy path", list: createToDoListWithItems(3, knownID), id: knownID, want: nil},
 		{name: "empty list", list: createtoDoList(), id: knownID, want: ErrNoItem},
-		{name: "item not in list", list: createToDoListWithItems(), id: uuid.New(), want: ErrNoItem},
+		{name: "item not in list", list: createToDoListWithItems(3, uuid.New()), id: uuid.New(), want: ErrNoItem},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -50,7 +97,7 @@ func TestCreateStorePopulatesList(t *testing.T) {
 		name string
 		list toDoList
 	}{
-		{name: "populated list", list: createToDoListWithItems()},
+		{name: "populated list", list: createToDoListWithItems(3, uuid.New())},
 		{name: "nil list", list: createtoDoList()},
 	}
 	for _, tt := range tests {
@@ -66,12 +113,14 @@ func TestCreateStorePopulatesList(t *testing.T) {
 
 var knownID = uuid.New()
 
-func createToDoListWithItems() toDoList {
-	return toDoList{
-		knownID:    createToDoItemPointer("banana", HighPriority),
-		uuid.New(): createToDoItemPointer("triple", MediumPriority),
-		uuid.New(): createToDoItemPointer("frog", LowPriority),
+func createToDoListWithItems(amount int, firstID uuid.UUID) toDoList {
+	toDoList := toDoList{
+		firstID: createToDoItemPointer("banana", HighPriority),
 	}
+	for i := 0; i < amount-1; i++ {
+		toDoList[uuid.New()] = createToDoItemPointer("banana"+strconv.Itoa(i), HighPriority)
+	}
+	return toDoList
 }
 
 func createToDoItemPointer(t string, p Priority) *toDoItem {
